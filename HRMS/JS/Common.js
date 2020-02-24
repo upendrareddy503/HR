a3759198962233056f4fa6597b97b8e05a5b8aa6
Upendra <uppendrareddy503@gmail.com>
2020-02-24 11:08:07 +0530

    common class

--- HRMS/JS/Common.js
+++ HRMS/JS/Common.js
@@ -20,3 +20,30 @@ function validateEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
 }
+
+function clearCSS() {
+    //$('select').each(function () {
+    $('.validate').css({
+        "border": "",
+        "background": ""
+    });
+    //});
+}
+
+function validate() {
+
+    var isValid = true;
+    $('.validate').each(function () {
+        if ($.trim($(this).val()) == '' || $.trim($(this).val()) == '0') {
+            isValid = false;
+            $(this).css("border", "1px solid red");
+        }
+        else {
+            $(this).css({
+                "border": "",
+                "background": ""
+            });
+        }
+    });
+    return isValid;
+}
